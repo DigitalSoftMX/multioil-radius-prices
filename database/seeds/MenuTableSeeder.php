3f4966abe88f4481a333a9bc051df961d0af6e71
Ricardo-Zavala <ricardo.zavala@digitalsoft.mx>
2021-04-12 12:55:35 -0500

    turnos

--- database/seeds/MenuTableSeeder.php
+++ database/seeds/MenuTableSeeder.php
@@ -14,7 +14,7 @@ class MenuTableSeeder extends Seeder
     {
 
         $menu = Menu::create(['name_module' => 'dashboard', 'display' => 0, 'route' => '/', 'id_role' => 0, 'icon' => 'nc-bank']);
-        $menu->roles()->sync([1, 2]);
+        $menu->roles()->sync([1, 2, 3]);
         $menu = Menu::create(['name_module' => 'Perfil', 'display' => 0, 'route' => 'profile', 'id_role' => 0, 'icon' => 'nc-single-02']);
         $menu->roles()->sync(1);
         $menu = Menu::create(['name_module' => 'Empresas', 'display' => 0, 'route' => 'companies', 'id_role' => 0, 'icon' => 'nc-tile-56']);
@@ -25,6 +25,10 @@ class MenuTableSeeder extends Seeder
         $menu->roles()->sync([1, 2]);
         $menu = Menu::create(['name_module' => 'Despachadores', 'display' => 0, 'route' => 'dispatchers', 'id_role' => 0, 'icon' => 'nc-badge']);
         $menu->roles()->sync(1);
+        $menu = Menu::create(['name_module' => 'Turnos', 'display' => 0, 'route' => 'schedules', 'id_role' => 0, 'icon' => 'nc-time-alarm']);
+        $menu->roles()->sync(3);
+        $menu = Menu::create(['name_module' => 'Islas y bombas', 'display' => 0, 'route' => 'islandbomb', 'id_role' => 0, 'icon' => 'nc-bullet-list-67']);
+        $menu->roles()->sync(3);
         $menu = Menu::create(['name_module' => 'Clientes', 'display' => 0, 'route' => 'clients', 'id_role' => 0, 'icon' => 'nc-bullet-list-67']);
         $menu->roles()->sync(1);
     }
