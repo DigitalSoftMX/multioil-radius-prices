4b3726d4e87ab18111f53f896d6cc4b186e97a48
ricardo.zavala@digitalsoft.mx <ricardo.zavala@digitalsoft.mx>
2021-05-28 15:27:15 -0500

    matricula

--- app/Client.php
+++ app/Client.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = ['user_id', 'membership', 'points', 'birthdate', 'sex', 'car', 'ids'];
+    protected $fillable = ['user_id', 'membership', 'points', 'birthdate', 'sex', 'ids'];
     // Relacion con el usuario
     public function user()
     {
